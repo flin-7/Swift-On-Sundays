bc23d45ccdd5aad5d6fe421fe716b2d2d3089d92
flin-7 <flin.7@icloud.com>
2019-11-15 11:28:13 -0800

    Add Timer to RGBullsEye

--- RGBullsEye/RGBullsEye/ContentView.swift
+++ RGBullsEye/RGBullsEye/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
     @State var gGuess: Double
     @State var bGuess: Double
     @State var showAlert = false
+    @ObservedObject var timer = TimeCounter()
     
     func computeScore() -> Int {
         let rDiff = rGuess - rTarget
@@ -26,32 +27,52 @@ struct ContentView: View {
     }
     
     var body: some View {
-        VStack {
-            HStack {
-                VStack {
-                    Color(red: rTarget, green: gTarget, blue: bTarget)
-                    Text("Match this color")
+        NavigationView {
+            VStack {
+                HStack {
+                    VStack {
+                        Color(red: rTarget, green: gTarget, blue: bTarget)
+                        self.showAlert ? Text("R: \(Int(rGuess * 255.0)) G: \(Int(gGuess * 255.0)) B: \(Int(bGuess * 255.0))") : Text("Match this color")
+                    }
+                    VStack {
+                        ZStack(alignment: .center) {
+                            Color(red: rGuess, green: gGuess, blue: bGuess)
+                            Text(String(timer.counter))
+                                .padding(.all, 5)
+                                .background(Color.white)
+                                .mask(Circle())
+                                .foregroundColor(.black)
+                        }
+                        Text("R: \(Int(rGuess * 255.0)) G: \(Int(gGuess * 255.0)) B: \(Int(bGuess * 255.0))")
+                    }
                 }
+                Button(action: {
+                    self.showAlert = true
+                    self.timer.killTimer()
+                }) {
+                    Text("Hit Me!")
+                }.alert(isPresented: $showAlert) {
+                    Alert(title: Text("Your Score"), message: Text(String(computeScore())))
+                }.padding()
+                
                 VStack {
-                    Color(red: rGuess, green: gGuess, blue: bGuess)
-                    Text("R: \(Int(rGuess * 255.0)) G: \(Int(gGuess * 255.0)) B: \(Int(bGuess * 255.0))")
-                }
+                    ColorSlider(value: $rGuess, textColor: .red)
+                    ColorSlider(value: $gGuess, textColor: .green)
+                    ColorSlider(value: $bGuess, textColor: .blue)
+                }.padding(.horizontal)
             }
-            Button(action: { self.showAlert = true }) {
-                Text("Hit Me!")
-            }.alert(isPresented: $showAlert) {
-                Alert(title: Text("Your Score"), message: Text(String(computeScore())))
-            }.padding()
-            ColorSlider(value: $rGuess, textColor: .red)
-            ColorSlider(value: $gGuess, textColor: .green)
-            ColorSlider(value: $bGuess, textColor: .blue)
+            .navigationBarTitle("", displayMode: .inline)
+            .navigationBarHidden(true)
         }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(rGuess: 0.8, gGuess: 0.5, bGuess: 0.5).previewLayout(.fixed(width: 568, height: 320))
+        ContentView(rGuess: 0.8, gGuess: 0.5, bGuess: 0.5)
+            .previewLayout(.fixed(width: 568, height: 320))
+//            .environment(\.colorScheme, .dark)
     }
 }
 
@@ -63,7 +84,9 @@ struct ColorSlider: View {
         HStack {
             Text("0").foregroundColor(textColor)
             Slider(value: $value)
+                .background(textColor)
+                .cornerRadius(10)
             Text("255").foregroundColor(textColor)
-        }.padding(.horizontal)
+        }
     }
 }
